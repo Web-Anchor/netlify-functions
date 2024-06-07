2908b7a456a9f966af3096d5e08994925fdc65ea
Ed <128392461+edtheanchor@users.noreply.github.com>
2024-06-07 10:12:01 +0100

    New commit message: snags to puppet api & convert to TS & add validations

---
+++ netlify/functions/pokedex.ts
@@ -0,0 +1,32 @@
+import {
+  allowedMethods,
+  requestBody,
+  validateAuthorization,
+} from '../../lib/helpers';
+
+const axios = require('axios');
+
+exports.handler = async (req, context) => {
+  try {
+    await allowedMethods({ method: req.httpMethod, allowedMethods: ['POST'] });
+    await validateAuthorization(req);
+    const body = requestBody(req);
+
+    const POKE_API = 'https://pokeapi.co/api/v2/pokedex/kanto';
+    const { data } = await axios.get(POKE_API);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'Pokedex',
+        data,
+        body,
+      }),
+    };
+  } catch (error) {
+    return {
+      statusCode: error?.statusCode ?? 500,
+      body: JSON.stringify({ message: error.message }),
+    };
+  }
+};
